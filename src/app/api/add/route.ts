b49fb7d865e1a14d5c458d796adf349c3637d2b0
AjithkumarMechon <ajithkumaranandandeveloper@gmail.com>
2025-07-31 13:23:55 +0530

    db changes

--- src/app/api/add/route.ts
+++ src/app/api/add/route.ts
@@ -1,14 +1,27 @@
 // app/api/add/route.ts
+import { postgresConnect } from "@/utils/dbConnect";
 import pool from "@/utils/postgresql";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import Pool from "@/utils/postgresql";
 
 const schema = z.object({
   name: z.string().min(2, "Name is too short"),
 });
 
+async function ensureTableExists() {
+  await Pool.query(`CREATE SCHEMA IF NOT EXISTS fullstacknextjs`);
+  await Pool.query(`CREATE TABLE IF NOT EXISTS fullstacknextjs."serverside" (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT null,
+  created_at TIMESTAMP DEFAULT NOW()
+ )
+  `);
+}
 export async function POST(req: NextRequest) {
   try {
+    await postgresConnect();
+    await ensureTableExists();
     const { name } = await req.json(); // because this is a form POST
 
     const result = schema.safeParse({ name });
